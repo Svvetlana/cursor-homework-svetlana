232ce0af5384567ac0fe7ac49d3c041d2f8a1622
Svvetlana <schsvetik74@gmail.com>
2021-06-06 21:01:13 +0300

    changes for support

--- 01-test-homework/01-test-homework.js
+++ 01-test-homework/01-test-homework.js
@@ -1,52 +1,56 @@
 //Base
-const apple = 15.678;
-const banana = 123.965;
-const pear = 90.2345;
+const applePrice = 15.678;
+const bananaPrice = 123.965;
+const pearPrice = 90.2345;
 
-//Максимальное число
-const priceMax = (Math.max(apple,banana,pear));
-console.log(`Максимальная цена:`+priceMax+`грн.`);
-//Минимальное число
-const priceMin = (Math.min(apple,banana,pear));
-console.log(`Минимальная цена:` + priceMin + `грн.`);
-//Сумма
-const suma = apple + banana + pear;
-console.log(`Сумма всех товаров:` + suma + `грн.`);
-//Цена товаров без копеек(округление в меньшую сторону) и их сумма
-const sumaFloor = Math.floor(apple) + Math.floor(banana) + Math.floor(pear);
-console.log(`Сумма всех товаров с ценой без копеек:` + sumaFloor + `грн.`);
-//Сумма, округленная до сотен
-const sumaRoundHundreds = (Math.round(sumaFloor/100))*100;
-console.log(`Сумма всех товаров, округленная до сотен:` + sumaRoundHundreds + `грн.`);
-//Булево значение
-const even = Math.floor(suma) %2 == 0;
-console.log(`Сумма всех товаров с ценой без копеек четное число?:` + even);
-//Сдача от 500 грн без округления цен
+
+const maxPrice = Math.max(applePrice, bananaPrice, pearPrice);
+console.log(`maxPrice`, maxPrice);
+
+const minPrice = Math.min(applePrice, bananaPrice, pearPrice);
+console.log(`minPrice`,  minPrice);
+
+const productsPrice = applePrice + bananaPrice + pearPrice;
+console.log(`producstPrice`, productsPrice);
+
+const productsPriceFloor = (
+    Math.floor(applePrice) + Math.floor(bananaPrice) + Math.floor(pearPrice)
+);
+console.log(`productsPriceFloor`, productsPriceFloor);
+
+const productsPriceRoundHundreds = (Math.round(productsPriceFloor / 100)) * 100;
+console.log(`productsPriceRoundHundreds`,  productsPriceRoundHundreds);
+
+const even = Math.floor(productsPrice) %2 === 0;
+console.log(`even`, even);
+
 const money = 500;
-const change = (money - suma).toFixed(2);
-console.log(`Сдача с 500 грн. без округления цен с двумя знаками после запятой:` + change + `грн.`);
-//Средняя цена, округленная до второго знака после запятой
-const sumaMiddle = (suma/3).toFixed(2);
-console.log(`Средняя цена всех товаров с двумя знаками после запятой:` + sumaMiddle + `грн.`);
-//Скидка, сумма к оплате(2 знака после запятой)
-const discount = Math.random();
-const sumaDiscount = (discount*100).toFixed(0);
-console.log(`Случайная скидка:` + sumaDiscount + `%`);
-const price = (suma - suma*sumaDiscount/100).toFixed(2);
-console.log(`Стоимость всех товаров со скидкой:` + price + `грн.`);
-//Чистая прибыль(себестоимость в 2 раза  ниже цены)
-const profit = suma/2 - sumaDiscount;
-console.log(`Чистая прибыль при себестоимости товаров в 2 раза ниже цены:` + profit + `грн.`);
+const change = (money - productsPrice).toFixed(2);
+console.log(`change`, change);
+
+const productsPriceMiddle = (productsPrice / 3).toFixed(2);
+console.log(`productsPriceMiddle`, productsPriceMiddle);
+
+
+const discount = (Math.random() * 100).toFixed(0);
+console.log(`discount`, discount, `%`);
+const priceWithDiscount = (
+    (productsPrice - productsPrice * discount / 100).toFixed(2)
+);
+console.log(`priceWithDiscount`, priceWithDiscount);
+
+const profit = productsPrice / 2 - discount;
+console.log(`profit`, profit);
 
 //Advansed
-document.write(`1.Максимальная цена: ${priceMax} грн.<br>
-2.Минимальная цена: ${priceMin} грн.<br>
-3.Сумма всех товаров: ${suma} грн.<br>
-4.Сумма всех товаров с ценой без копеек: ${sumaFloor} грн.<br>
-5.Сумма всех товаров, округленная до сотен: ${sumaRoundHundreds} грн.<br>
+document.write(`1.Максимальная цена: ${maxPrice} грн.<br>
+2.Минимальная цена: ${minPrice} грн.<br>
+3.Сумма всех товаров: ${productsPrice} грн.<br>
+4.Сумма всех товаров с ценой без копеек: ${productsPriceFloor} грн.<br>
+5.Сумма всех товаров, округленная до сотен: ${productsPriceRoundHundreds} грн.<br>
 6.Сумма всех товаров с ценой без копеек четное число?: ${even}<br>
 7.Сдача с 500 грн. без округления цен с двумя знаками после запятой: ${change} грн.<br>
-8.Средняя цена всех товаров с двумя знаками после запятой: ${sumaMiddle} грн.<br>
-9.Случайная скидка: ${sumaDiscount} %<br>
-10.Стоимость всех товаров со скидкой: ${price} грн.<br>
+8.Средняя цена всех товаров с двумя знаками после запятой: ${productsPriceMiddle} грн.<br>
+9.Случайная скидка: ${discount} %<br>
+10.Стоимость всех товаров со скидкой: ${priceWithDiscount} грн.<br>
 11.Чистая прибыль при себестоимости товаров в 2 раза ниже цены: ${profit} грн.`)
