bb456ae77b0949838abc8199ef5f94d9440e9a3e
Svvetlana <schsvetik74@gmail.com>
2021-06-09 21:32:39 +0300

    changes to my second homework

--- 02-test-homework/02-test-homework.js
+++ 02-test-homework/02-test-homework.js
@@ -1,24 +1,22 @@
-let N = prompt(`Введите первое целое число`);
+let firstNumber = null;
+let secondNumber = null;
+do {
+  firstNumber = prompt(`Введите первое целое число`);  
+} while ((isNaN(firstNumber) || !(firstNumber % 1 === 0) || !firstNumber));
 
-while (isNaN(N) || !(N % 1 === 0) || !N) {
-  N = prompt(`Это неверное число! Попробуйте ещё раз.`);
-}
+do {
+secondNumber = prompt(`Введите второе целое число`);
+} while ((isNaN(secondNumber) || !(secondNumber % 1 === 0) || !secondNumber));
 
-let M = prompt(`Введите второе целое число`);
-
-while (isNaN(M) || !(M % 1 === 0) || !M) {
-  M = prompt(`Это неверное число! Попробуйте ещё раз.`);
-}
-
-const even = confirm(`Будем ли пропускать четные числа?`);
+const isEven = confirm(`Будем ли пропускать четные числа?`);
 let suma = 0;
-let minNumber = Math.min(N, M);
-let maxNumber = Math.max(N, M);
+const minNumber = Math.min(firstNumber, secondNumber);
+const maxNumber = Math.max(firstNumber, secondNumber);
 
-for (minNumber; minNumber <= maxNumber; minNumber ++ ) {
-  if (even && minNumber %2 === 0) {
-    continue;
+for (let i = minNumber; i <= maxNumber; i ++ ) {
+  if (!isEven || i %2 !== 0) {
+  suma += i;
   }
-  suma += minNumber;
 }
+
 document.write(`Сумма всех чисел равна: ${suma}`);
