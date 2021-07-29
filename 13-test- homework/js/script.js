7b69de06ad7e10756005f3ac1811d45d54c08d2f
Svvetlana <schsvetik74@gmail.com>
2021-07-29 21:59:50 +0300

    13-test-homeworkadded

---
+++ 13-test- homework/js/script.js
@@ -0,0 +1,76 @@
+const characterInfo = document.getElementById('character-info');
+const buttonGetCharacterInfo = document.getElementById('getCharacterInfo');
+const planetInfo = document.getElementById('planet-info');
+const buttonGetPlanetInfo = document.getElementById('getPlanetInfo');
+const buttonNext = document.getElementById('next');
+const buttonPrev = document.getElementById('prev');
+
+async function getAboutFilmCharacterInfo(url) {
+    request(url)
+        .then(data => {
+            return data.characters;
+        })
+        .then(character => {
+            characterInfo.innerHTML = '';
+            planetInfo.innerHTML = '';
+            return character.map(e => getCharacterInfo(e));
+        });
+}
+
+async function getAboutPlanetInfo(url) {
+    request(url)
+        .then(data => {
+            return data.results;
+        })
+        .then(results => {
+            characterInfo.innerHTML = '';
+            planetInfo.innerHTML = '';
+            return results.map(e => getPlanetInfo(e));
+        });
+}
+
+async function getCharacterInfo(url) {
+    request(url).then(info => {
+        return (
+            characterInfo.innerHTML += `<div class="character-info">
+            <p>${info.name}</p><img src="img/${info.name}.jpg"/> 
+			<p>Birth_year ${info.birth_year}</p>
+			<p>Gender ${info.gender} </p></div>`
+        );
+    });
+}
+
+async function getPlanetInfo(info) {
+    return planetInfo.innerHTML += `<li class="planet-info"><p> ${info.name}</p></li>`;
+}
+
+async function request(url) {
+    return fetch(url)
+        .then(response => response.json())
+        .catch(console.log);
+}
+
+buttonGetCharacterInfo.addEventListener('click', () => {
+    getAboutFilmCharacterInfo(`https://swapi.dev/api/films/2`);
+});
+
+buttonGetPlanetInfo.addEventListener('click', () => {
+    getAboutPlanetInfo(`https://swapi.dev/api/planets/`);
+});
+
+let page = 1;
+buttonNext.addEventListener('click', e => {
+    page = page + 1;
+    if (page > 6) {
+        page = 6;
+    }
+    getAboutPlanetInfo(`https://swapi.dev/api/planets/?page=${page}`);
+});
+
+buttonPrev.addEventListener('click', () => {
+    page = page - 1;
+    if (page < 1) {
+        page = 1;
+    }
+    getAboutPlanetInfo(`https://swapi.dev/api/planets/?page=${page}`);
+});
